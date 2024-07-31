e6359e19dd2247d4244aeccf02a02f2f229aa12e
JohnOmondi <jomondi518@gmail.com>
2024-07-31 08:39:57 +0300

    initial commit

---
+++ src/forloop.kt
@@ -0,0 +1,33 @@
+fun main() {
+
+    for (num in 1..5) {
+
+        println("Number :$num")
+    }
+
+    for (letter in 'a'..'f') {
+
+        println("letter is $letter")
+
+    }
+
+   for (mynumber  in 185..190){
+       if (mynumber == 187){
+           continue
+       }
+       println(mynumber)
+   }
+
+for (myletter in 'a'..'d'){
+    if (myletter =='b'){
+        continue
+    }
+    println(myletter)
+}
+
+}
+
+
+
+
+
---
+++ src/functions.kt
@@ -0,0 +1,70 @@
+fun main(){
+    //predefined function
+    println("eMobilis")
+
+    var squareroot = Math.sqrt(81.0)
+    println("the square root is $squareroot ")
+
+
+    var round =Math.round(32.67)
+    println("the result is $round")
+
+
+    var myceil = Math.ceil(45.21)
+    println("the output value is $myceil")
+
+    day()
+    add()
+
+    student("John")
+
+
+
+}
+
+
+// user define functions
+
+fun day() {
+
+    println("Today is Tuesday")
+
+}
+
+fun add() {
+    var num1 = 56
+    var num2 = 100
+
+println( num1 + num2)
+
+
+}
+
+//parameter and argument
+
+fun student(name: String ,) {
+    println("$name is three years old")
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
